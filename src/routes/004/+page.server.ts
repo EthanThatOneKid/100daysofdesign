a347fea15801eb4dc816736f3a3f97be1f5d80eb
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-21 22:52:39 -0700

    calc is short for calculator just slang guys

--- src/routes/004/+page.server.ts
+++ src/routes/004/+page.server.ts
@@ -3,6 +3,6 @@ import { getFollowRatio } from "./github";
 export async function load({ url }: { url: URL }) {
   const username = url.searchParams.get("username");
   return {
-    followRatio: username !== null ? await getFollowRatio(username) : null,
+    followRatio: username ? await getFollowRatio(username) : null,
   };
 }
