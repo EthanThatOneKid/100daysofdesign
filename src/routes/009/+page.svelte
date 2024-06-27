025620683f0480cf3e594d485637beb1cacddd57
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-27 00:42:00 -0700

    Day 9 What Was Missing
    Add BMO music player from Adventure Time episode [What Was Missing](https://adventuretime.fandom.com/wiki/What_Was_Missing).

---
+++ src/routes/009/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Music from "./music.svelte";
+  import referencePNG from "./reference.png";
+</script>
+
+<Music />
+
+<div class="flex items-center justify-center h-screen">
+  <div
+    class="relative w-[800px] h-[600px] bg-background border border-input rounded-lg shadow-lg"
+  >
+    <a href="https://adventuretime.fandom.com/wiki/What_Was_Missing"
+      ><img
+        src={referencePNG}
+        alt="Reference used to design the user profile"
+        width="800"
+        height="600"
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full object-cover"
+        style="aspect-ratio:600/400;object-fit:cover"
+      /></a
+    >
+  </div>
+</div>
---
+++ src/routes/009/music.svelte
@@ -0,0 +1,184 @@
+<script lang="ts">
+  import circuitSVG from "./circuit.svg";
+
+  const notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
+
+  let audioCtx: AudioContext | null = null;
+  let recentNote: string | null = null;
+
+  function handleMouseEvent(event: MouseEvent) {
+    if (!audioCtx) {
+      audioCtx = new AudioContext();
+    }
+
+    const percentage = event.clientX / event.target?.clientWidth;
+    playNote(percentage);
+  }
+
+  function playNote(percentage: number) {
+    const index = Math.floor(percentage * notes.length);
+    const note = notes[index];
+    recentNote = note;
+
+    if (note && audioCtx) {
+      const oscillator = audioCtx.createOscillator();
+      oscillator.type = "sine";
+      oscillator.frequency.setValueAtTime(
+        getFrequency(note),
+        audioCtx.currentTime
+      );
+
+      const gainNode = audioCtx.createGain();
+      gainNode.gain.setValueAtTime(0.1, audioCtx.currentTime); // Adjust volume
+
+      oscillator.connect(gainNode);
+      gainNode.connect(audioCtx.destination);
+
+      oscillator.start(audioCtx.currentTime);
+      oscillator.stop(audioCtx.currentTime + 0.2); // Adjust duration
+    }
+  }
+
+  function getFrequency(note: string) {
+    const octave = parseInt(note.slice(-1));
+    const baseNote = note.slice(0, -1);
+    let frequency = 0;
+
+    switch (baseNote) {
+      case "C":
+        frequency = 261.63;
+        break;
+      case "D":
+        frequency = 293.66;
+        break;
+      case "E":
+        frequency = 329.63;
+        break;
+      case "F":
+        frequency = 349.23;
+        break;
+      case "G":
+        frequency = 392;
+        break;
+      case "A":
+        frequency = 440;
+        break;
+      case "B":
+        frequency = 493.88;
+        break;
+    }
+
+    return frequency * Math.pow(2, octave - 4);
+  }
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+  class="__className_aaf875"
+  on:mousemove={handleMouseEvent}
+  style:background-image="url({circuitSVG})"
+>
+  <div class="bmo_screen__jgxgK">
+    <div class="bmo_face__I_Ksk">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"
+        ><circle
+          cx="33"
+          cy="30"
+          r="2"
+          fill="black"
+          data-darkreader-inline-fill=""
+          style="--darkreader-inline-fill: #e7e6e5;"
+        ></circle><circle
+          cx="66"
+          cy="30"
+          r="2"
+          fill="black"
+          data-darkreader-inline-fill=""
+          style="--darkreader-inline-fill: #e7e6e5;"
+        ></circle><path
+          d="M 35 45 Q 50 55 65 45"
+          stroke="black"
+          stroke-width="2"
+          fill="transparent"
+          stroke-linecap="round"
+          data-darkreader-inline-stroke=""
+          style="--darkreader-inline-stroke: #e7e6e5; --darkreader-inline-fill: transparent;"
+          data-darkreader-inline-fill=""
+        ></path>
+
+        <text x="50" y="80" text-anchor="middle" font-size="10"
+          >{#if recentNote}BMO sings {recentNote}{:else}Headphone warning!{/if}</text
+        >
+      </svg>
+    </div>
+  </div>
+</div>
+
+<style>
+  .__className_aaf875 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+
+  .bmo_screen__jgxgK {
+    width: min(600px, 100%);
+    height: 400px;
+    background-color: #bffecb;
+    border-radius: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    outline: 0.5rem solid #589c85;
+    @media (max-width: 600px) {
+      margin-top: -50vh;
+    }
+  }
+  .bmo_face__I_Ksk svg {
+    width: 100%;
+    height: 100%;
+  }
+  .bmo_message__N3J_2 {
+    margin: 1rem;
+  }
+  .bmo_message__N3J_2 pre,
+  .bmo_systemPrompt__rg3sB {
+    white-space: pre-wrap;
+  }
+  .bmo_sidebar__wdjTK {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    width: 300px;
+    background-color: rgba(0, 0, 0, 0.25);
+    overflow: auto;
+    border-radius: 1rem;
+    padding: 1.25rem;
+    margin: 0.25rem;
+    @media (max-width: 600px) {
+      top: 50%;
+      margin-left: 0;
+      margin-bottom: 0;
+      margin-right: 0;
+      margin-top: 0;
+      width: 100%;
+    }
+  }
+  .bmo_sidebar__wdjTK hr {
+    margin: 0.5rem 0;
+  }
+  .bmo_sidebar__wdjTK ul {
+    margin-left: -1rem;
+  }
+  .bmo_chatInput__MbJx2 {
+    width: 100%;
+  }
+  .bmo_github__eP9Az {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    padding: 0.5rem;
+  }
+</style>
