10161e48d1686c798b00051619ed2a55b73104bb
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-07-04 14:40:43 -0700

    add day 14 submission

---
+++ src/routes/014/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import pomoPNG from "./pomo.png";
+</script>
+
+<div class="flex items-center justify-center">
+  <div class="relative bg-background border border-input rounded-lg shadow-lg">
+    <a href="https://acmcsuf.com/pomo"
+      ><img src={pomoPNG} alt="Screenshot of Pomo" /></a
+    >
+  </div>
+</div>
+
+<p class="text-lg mt-4">
+  Screenshot of the <a
+    href="https://acmcsuf.com/pomo"
+    class="text-blue-500 underline">Pomo</a
+  > app.
+</p>
