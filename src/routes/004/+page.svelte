e5233a149eb004fe0c83d6c23d953e4b2324c4dd
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-07-04 14:27:00 -0700

    link GitHub Follower to Following Ratio Calculator

--- src/routes/004/+page.svelte
+++ src/routes/004/+page.svelte
@@ -3,3 +3,12 @@
 </script>
 
 <Calc />
+
+<p class="text-lg mt-4">
+  Additional calculator: <a
+    href="https://github.com/FartLabs/github-follow-ratio"
+    class="text-blue-500 underline"
+  >
+    GitHub Follower to Following Ratio Calculator</a
+  >.
+</p>
