33e857efff7d5b680751da429eb49cb1dac68f51
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-07-04 13:24:44 -0700

    retroactively submit prior art for day 7

---
+++ src/routes/007/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import denoBlocksSettingsPNG from "./deno_blocks_settings.png";
+</script>
+
+<div class="flex items-center justify-center">
+  <div class="relative bg-background border border-input rounded-lg shadow-lg">
+    <a href="https://github.com/FartLabs/deno_blocks"
+      ><img
+        src={denoBlocksSettingsPNG}
+        alt="Screenshot of Deno Blocks settings"
+      /></a
+    >
+  </div>
+</div>
+
+<p class="text-lg mt-4">
+  Screenshot of the <a
+    href="https://github.com/FartLabs/deno_blocks"
+    class="text-blue-500 underline">Deno Blocks</a
+  > settings page.
+</p>
