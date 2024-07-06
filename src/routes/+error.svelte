526d03072f20edbe5ef9f361829ea8b6ebe0014d
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-07-06 15:55:28 -0700

    Create +error.svelte
    https://kit.svelte.dev/docs/errors

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<h1 class="text-4xl">
+  {#if $page.status === 404}
+    Ethan is an L bozo! 404 🤡
+  {:else}
+    {$page.status}
+  {/if}
+</h1>
+
+<p>{$page.error?.message}</p>
