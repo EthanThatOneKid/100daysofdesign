aac3bc3c3f2e83822ff45955f958fed79ae0384d
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-07-06 15:58:49 -0700

    Update +page.svelte

--- src/routes/015/+page.svelte
+++ src/routes/015/+page.svelte
@@ -3,3 +3,6 @@
 </script>
 
 <Switch />
+
+<!-- TODO: Idea is to turn rising lands into a big switch.
+https://github.com/FartLabs/fart.tools/blob/06d99a756b693ee2c0ef909d5d9d24d38e2305a5/components/landing_page/rise_section.tsx -->
